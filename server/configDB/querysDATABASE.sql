9f51ad90ae057d127c6256a65c9148964a6bfe59
Michael Briceno <michael@lioneve.com>
2022-10-26 20:50:17 -0400

    Refactory tables

--- server/configDB/querysDATABASE.sql
+++ server/configDB/querysDATABASE.sql
@@ -4,17 +4,15 @@ CREATE TABLE personal (
     id SERIAL NOT NULL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE,
-    rol VARCHAR(50) NOT NULL,
-    cedula VARCHAR(14) NOT NULL;
-    area_personal VARCHAR(50),
+    rol VARCHAR(50) NOT NULL, /* director - docente - especialista - coordinador */
+    cedula VARCHAR(14),
+    especialidad VARCHAR(50), /* area_personal cambiar a especialidad en el back */
     claveuser VARCHAR(100),
-    fecha_reg DATE NOT NULL
+    fecha_reg TIMESTAMPTZ DEFAULT Now()
 );
 
-INSERT INTO personal(nombre, email, rol, cedula,area_personal, claveuser, estado, fecha_reg) 
-VALUES('Alberto Jesus Graterol Flores','algcmusic18@gmail.com','departamento de evaluacion', '2787564',
-	   'Directivo', 'clave8346', 'Activo', DATE '26-07-2021');
-
+INSERT INTO personal(nombre, email, rol, cedula, especialidad, claveuser) 
+VALUES('Alberto Jesus Graterol Flores','algcmusic18@gmail.com','coordinador', '12345678','musica', 'clave1234');
 
 
 CREATE TABLE estudiante (
@@ -22,8 +20,8 @@ CREATE TABLE estudiante (
     cedula_escolar VARCHAR(20) NOT NULL UNIQUE,
     nombres VARCHAR(255) NOT NULL,
     genero VARCHAR(50),
-    grado VARCHAR(50) NOT NULL,
-    seccion VARCHAR(10) NOT NULL,
+    grado VARCHAR(50) NOT NULL, /* nivel 1, nivel 2  y nivel 3 */
+    seccion VARCHAR(10),
     boleta_generada VARCHAR(15)
 );
 
@@ -33,10 +31,10 @@ CREATE TABLE indicador (
     momento VARCHAR(17) NOT NULL,
     area VARCHAR(50) NOT NULL,
     condicion_especial VARCHAR(10),
-    id_creador VARCHAR(6) NOT NULL,
-    grado VARCHAR(9),
-    literal VARCHAR(10), 
-    fecha_creacion VARCHAR(8) NOT NULL,
+    nombre_docente VARCHAR(80) NOT NULL,
+    grado VARCHAR(9), /* nivel 1, nivel 2  y nivel 3 */
+    literal VARCHAR(10), /*  muy bien, bien, en proceso, requiere nivelación */
+    fecha_creacion VARCHAR(8) NOT NULL
 );
 
 
@@ -49,15 +47,15 @@ CREATE TABLE boleta (
     momento VARCHAR(10) NOT NULL,
     nombre_estudiante VARCHAR NOT NULL,
     nombre_docente VARCHAR NOT NULL,
-    cedula_estudiante VARCHAR NOT NULL,
-    inicio_momemnto VARCHAR NOT NULL,
-    fin_momento VARCHAR NOT NULL,
+    cedula_estudiante VARCHAR,
+    mes_momento_inicio VARCHAR NOT NULL,
+    mes_momento_fin  VARCHAR NOT NULL,
     fecha_de_creacion VARCHAR NOT NULL
 );
 
 CREATE TABLE materias (
     id SERIAL NOT NULL PRIMARY KEY,
-    materia VARCHAR(35) NOT NULL,
-    tipo VARCHAR(18) NOT NULL,
+    materia VARCHAR(100) NOT NULL,
+    tipo VARCHAR(30) NOT NULL
 );
 
